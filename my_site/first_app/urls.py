a7c3bfe2f23b5f1e916739dbb484d8ca22c4e467
Cedric Kiplimo <kiplimock@gmail.com>
2022-12-04 22:46:05 +0300

    first template

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 # this is a list of view routes
 urlpatterns = [
     # path() connects a view function to a URL
-    path('<int:num_page>/', views.num_page_view),
-    path('<str:topic>/', views.news_view, name='topic_page')
+    path('', views.simple_view) # domain.com/first_app
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -2,22 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 
-articles = {
-    'sports': 'Sports Page',
-    'finance': 'Finance Page',
-    'politics': 'Politics Page'
-}
-# Create your views here.
-def news_view(request, topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(result)
-    except:
-        raise Http404("404 Generic Error")
+# Create your views here 
 
-# domain.com/first_app/0 -----> doain.com/first_app/sports
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys()) # ['sports', 'finance', 'politics']
-    topic = topics_list[num_page]
-    
-    return HttpResponseRedirect(reverse('topic_page', args=[topic]))
+def simple_view(request):
+    return render(request, 'first_app/example.html')
