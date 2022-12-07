d0067590862eda181c13187807fec655446f8449
Cedric Kiplimo <kiplimock@gmail.com>
2022-12-07 23:47:10 +0300

    context variables

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def example_view(request):
     return render(request, 'my_app/example.html')
+
+def variable(request):
+
+    context = {
+        'first_name': 'Cedric', 
+        'last_name': 'Kiplimo',
+        'my_list': [1,2,3]
+    }
+    return render(request, 'my_app/variable.html', context=context)
