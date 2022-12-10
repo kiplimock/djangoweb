cf373262f738064aabba08ea2e780f67ce5599d7
Cedric Kiplimo <kiplimock@gmail.com>
2022-12-10 23:31:08 +0300

    django for loop tags

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -9,6 +9,7 @@ def variable(request):
     context = {
         'first_name': 'Cedric', 
         'last_name': 'Kiplimo',
-        'my_list': [1,2,3]
+        'my_list': [1,2,3],
+        'some_dict': {'a': 1, 'b': 2, 'c': 3}
     }
     return render(request, 'my_app/variable.html', context=context)
