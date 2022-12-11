756c057d7115dbbab43e4b49e79680ade02732be
Cedric Kiplimo <kiplimock@gmail.com>
2022-12-11 18:10:05 +0300

    added template inheritance

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def example_view(request):
+    
     return render(request, 'my_app/example.html')
 
 def variable(request):
 
-    context = {
-        'first_name': 'Cedric', 
-        'last_name': 'Kiplimo',
-        'my_list': [1,2,3],
-        'user_logged_in': False,
-    }
-    return render(request, 'my_app/variable.html', context=context)
+    return render(request, 'my_app/variable.html')
