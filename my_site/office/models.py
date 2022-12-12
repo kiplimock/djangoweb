829870469a01f74686e0d37f964fb3cb58b0dfb3
Cedric Kiplimo <kiplimock@gmail.com>
2022-12-12 17:55:59 +0300

    querying objects with .all method

--- my_site/office/models.py
+++ my_site/office/models.py
@@ -5,4 +5,7 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
-    
+
+    def __str__(self):
+
+        return f"{self.first_name} {self.last_name} {self.age}"
