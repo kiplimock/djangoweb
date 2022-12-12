c9812eafe16642cd4a42a9a249134f09c77059d0
Cedric Kiplimo <kiplimock@gmail.com>
2022-12-12 17:27:35 +0300

    first migrations

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'office.apps.OfficeConfig',
     'my_app.apps.MyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
