20a132dc611831ed9303b237aa8b3f8a2eefe1f6
Cedric Kiplimo <kiplimock@gmail.com>
2023-01-03 21:22:09 +0300

    model forms and customization

--- car_rental_site/cars/forms.py
+++ car_rental_site/cars/forms.py
@@ -1,10 +1,27 @@
 from django import forms
+from .models import Review
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100,
-                                widget=forms.TextInput(attrs={'class':'form-control'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),
-                                label='Last Name', max_length=100)
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'e.g., john.doe@domain.com'}))
-    review = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}),
-                             label='Leave us your thoughts here')
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label='First Name', max_length=100,
+#                                 widget=forms.TextInput(attrs={'class':'form-control'}))
+#     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),
+#                                 label='Last Name', max_length=100)
+#     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'e.g., john.doe@domain.com'}))
+#     review = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}),
+#                              label='Leave us your thoughts here')
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review 
+        fields = "__all__"
+        labels = {
+            'first_name': 'Your First Name',
+            'last_name': 'Your Last Name',
+            'stars': 'Rating'
+        }
+        error_messages = {
+            'stars': {
+                'min_value':"Yo! Min value is 1",
+                'max_value':"Yo! Yo! Max value is 5"
+            }
+        }
--- car_rental_site/cars/models.py
+++ car_rental_site/cars/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Review(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    stars = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
