dfaf0e15458a609a67d7772f7dd766928a6d57c6
Cedric Kiplimo <kiplimock@gmail.com>
2023-01-03 18:45:35 +0300

    django form templating

--- car_rental_site/cars/forms.py
+++ car_rental_site/cars/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100)
+    first_name = forms.CharField(label='First Name', max_length=100, attrs={'class': 'form-control'})
     last_name = forms.CharField(label='Last Name', max_length=100)
     email = forms.EmailField(label='Email')
     review = forms.CharField(label='Leave us your thoughts here')
-
