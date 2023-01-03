714bbefed48d3f9553a3f5c5109189427d17cdd7
Cedric Kiplimo <kiplimock@gmail.com>
2023-01-03 20:14:17 +0300

    widgets and styling

--- car_rental_site/cars/forms.py
+++ car_rental_site/cars/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100, attrs={'class': 'form-control'})
-    last_name = forms.CharField(label='Last Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    review = forms.CharField(label='Leave us your thoughts here')
+    first_name = forms.CharField(label='First Name', max_length=100,
+                                widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),
+                                label='Last Name', max_length=100)
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'e.g., john.doe@domain.com'}))
+    review = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}),
+                             label='Leave us your thoughts here')
